4a326ac5abbf896cfccea5655a676b00e81f19cc
jhuband <jmh5ad@virginia.edu>
2022-06-21 16:07:20 -0400

    Update data & code

--- app.R
+++ app.R
@@ -17,7 +17,10 @@ ui <- fluidPage(
                               "New Rivanna Researchers"='h',
                               "Ivy Researchers"='c', 
                               "Ivy Projects"='e',
-                              "Storage Usage"='d')), 
+                              "Ivy Storage"= 'i',
+                              "Standard Storage"= 'j',
+                              "Project Storage"= 'k'))
+                              #"Storage Usage"='d')), 
 
         ),
         mainPanel(       
@@ -29,6 +32,7 @@ ui <- fluidPage(
     
 gen_pie_chart <- function(filename){
     
+    
     school_ndx <- list(BII = 1,
                        CLAS = 2, 
                        COMM = 3,
@@ -39,11 +43,12 @@ gen_pie_chart <- function(filename){
                        SEAS = 8,
                        SEHD = 9, 
                        SOM = 10,
-                       "VPR/VPAT" = 11, 
+                       "VPR/PROV" = 11, 
                        NUR = 12,
                        BATT = 13,
                        ARCH = 14,
-                       MC = 15
+                       MC = 15,
+                       ACCORD = 16
     )
     
     
@@ -57,11 +62,12 @@ gen_pie_chart <- function(filename){
                      "SEAS",
                      "SEHD", 
                      "SOM",
-                     "VP/VPAT", 
+                     "VPR/PROV", 
                      "NUR",
                      "BATT",
                      "ARCH",
-                     "MC"
+                     "MC", 
+                     "ACCORD"
     )
     all_colors <- c("#FFE699",
                     "#9DC3E6", 
@@ -77,39 +83,49 @@ gen_pie_chart <- function(filename){
                     "#C45911",
                     "#9999FF",
                     "#F73B90", 
-                    "#0066FF")
+                    "#0066FF", 
+                    "#0099CC")
 
     raw_data <- read_excel(filename)
-    
     raw_data <- raw_data %>% filter(Count > 0) %>%
         mutate(Percent = 100*Count/sum(Count))
-    low_ndx <- which(raw_data$Percent < 1.5)
+    
+    Other_count <- max(0, (raw_data %>% filter(School=="Other"))$Count)
+    Other_percent <- max(0, (raw_data %>% filter(School=="Other"))$Percent)    
+    ## Keep only the schools that have a count greater than 0
+    ## Compute percentages based on counts
+
+    raw_data <- raw_data %>% filter(!School=="Other")
+    ## Identify the schools that have a low percentage
+    ## For ones with low percentage,combine the scores with Other
+    low_ndx <- which(raw_data$Percent <= 1.5)
     if (length(low_ndx) > 0){
         
-        Other_count <- sum(raw_data$Count[low_ndx])
+        Other_count <- Other_count + sum(raw_data$Count[low_ndx])
+        Other_percent <- Other_percent + sum(raw_data$Percent[low_ndx])
         
-        
+        ## Remove the low count data from the table
         raw_data <- raw_data %>% filter(Percent > 1.5) 
-        other_data <- raw_data %>% filter(grepl("Other", School))
-        if (nrow(other_data) > 0){
-            Other_count <- Other_count + as.numeric(other_data$Count)
-        }
-        Other_percent <- 100*Other_count/sum(raw_data$Count) 
-        raw_data <- raw_data %>% filter(!grepl("Other", School)) %>%
-            add_row(School="Other", Count=Other_count, Percent=Other_percent)
-        
         
     }
 
+    ## Check if "Other" is too low to report
 
+    if (Other_percent > 0.6){
+        raw_data <- raw_data %>% add_row(School="Other",Count=Other_count, Percent=Other_percent)
+        
+    }
         
 
     N <- nrow(raw_data)
     
     schools <- raw_data$School
     values <- raw_data$Count
+    #schools <- c(schools[1:3], schools[5], schools[4], schools[6])
+    #values <- c(values[1:3], values[5], values[4], values[6])
     
     ndx <- as.numeric(unlist(school_ndx[schools]))
+    
     total <- sum(values)
     
     percent <- round(100*values/total,0)
@@ -128,20 +144,27 @@ gen_pie_chart <- function(filename){
     monthNum <- as.numeric(date_parts[2])
     myMonth <- month.name[monthNum]
     month <- paste(myMonth, myYear)
-    titles <- list("Consultations"= paste0("Consultations by School\n",month, "\n"),
+    titles <- list("Consultations"= paste0("Advanced Consultation Projects by School\n",month, "\n"),
                    "Office_Hours" = paste0("Office Hour Attendees by School\n",month, "\n"),
                    "Ivy_Users"= paste("Ivy Researchers\n", month, "\n"),
                    "Ivy_Dist" = paste0("Ivy Projects\n", month, "\n"),
                    "Data/Storage.xlsx"="Storage usage by School",
                    "Rivanna_Allocations"=paste0("Rivanna Allocation Requests\n", month, "\n"),
                    "Core_Hours"= paste0("Rivanna Usage: Core Hours\n",month, "\n"),
+                   "Ivy_Storage_Summary"=paste0("Ivy Storage usage by School\n", month, "\n"),
+                   "Standard_Storage_Summary"=paste0("Research Standard Storage usage by School\n", month, "\n"),
+                   "Project_Storage_Summary"=paste0("Research Project Storage usage by School\n", month, "\n"),
                    "New_Users"=paste0("New Rivanna Researchers\n", month, "\n"))
+    
     if (file_type == "Rivanna_Allocations" || 
         file_type == "Core_Hours" ||
         file_type == "New_Users" ||
         file_type == "Ivy_Users" ||
         file_type == "Ivy_Dist"  ||
         file_type == "Office_Hours" ||
+        file_type == "Ivy_Storage_Summary" ||
+        file_type == "Standard_Storage_Summary" ||
+        file_type == "Project_Storage_Summary" ||
         file_type == "Consultations"){
         chart_title <- as.character(titles[file_type])    
        
@@ -207,7 +230,7 @@ server <- function(input, output) {
                 filename <- files[ndx[1]]
                 gen_pie_chart(filename)
             }
-            #gen_pie_chart("Data/Office_Hours.xlsx")
+            
         } 
         if(input$p=='g'){ 
             ndx <- grep("Core_Hours", files)
@@ -216,14 +239,34 @@ server <- function(input, output) {
                 gen_pie_chart(filename)
             }
             #gen_pie_chart("Data/Core_Hours.xlsx")
-        } 
+        }
         if(input$p=='h'){ 
             ndx <- grep("New_Users", files)
             if (length(ndx) > 0){
                 filename <- files[ndx[1]]
                 gen_pie_chart(filename)
             }
-            #gen_pie_chart("Data/New_Users.xlsx")
+        }
+        if(input$p=='i'){ 
+            ndx <- grep("Ivy_Storage", files)
+            if (length(ndx) > 0){
+                filename <- files[ndx[1]]
+                gen_pie_chart(filename)
+            }
+        } 
+        if(input$p=='j'){ 
+            ndx <- grep("Standard_Storage", files)
+            if (length(ndx) > 0){
+                filename <- files[ndx[1]]
+                gen_pie_chart(filename)
+            }
+        } 
+        if(input$p=='k'){ 
+            ndx <- grep("Project_Storage", files)
+            if (length(ndx) > 0){
+                filename <- files[ndx[1]]
+                gen_pie_chart(filename)
+            }
         } 
  
         }) 
